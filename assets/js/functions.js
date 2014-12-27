947a59f545625c72c73fc002250276e1388ceb71
SaraMalaney <sara.m.greenberg@gmail.com>
2014-12-27 00:03:49 -0400

    not working ajax request

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -12,7 +12,7 @@
 
 	/* trigger when page is ready */
 	$(document).ready(function (){
-		initialize()
+		//initialize()
 		// your functions go here
 	
 	});
@@ -47,5 +47,60 @@ function getDirectionsMap(){
 	iframe.width= '400px';
 	iframe.height= '400px';
     iframe.src = 'https://www.google.com/maps/embed/v1/directions?key=AIzaSyBDezljMuJwKTS4kGOC8_V3Br9ME1tb-5Y&origin=' + x.elements[0].value + '&destination='+x.elements[1].value;
-    document.body.appendChild(iframe);
+    document.getElementById("container-left").appendChild(iframe);
+
+
+    // Request the JSON and process it
+  /*  $.ajax({
+        type:'GET',
+        url:"https://www.maps.googleapis.com/maps/api/directions/json",
+        data:"origin=Toronto&destination=Montreal&key=AIzaSyBDezljMuJwKTS4kGOC8_V3Br9ME1tb-5Y",
+        success:function(feed) {
+        	alert("yes!");
+ 
+            // Loop through the items
+            //for(var i=0, l=feed.items.length; i < l && i < 16;   i) 
+            //{
+               
+            //}
+ 
+            // Display the thumbnails on the page
+            //$('#feed').html(thumbs.join(''));
+
+        },
+        dataType:'json'
+    });
+*/
+    $.ajax({
+		type: 'GET',
+		url: 'https://www.maps.googleapis.com/maps/api/directions/json',
+		contentType: "text/plain",
+
+	  xhrFields: {
+	    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+	    // This can be used to set the 'withCredentials' property.
+	    // Set the value to 'true' if you'd like to pass cookies to the server.
+	    // If this is enabled, your server must respond with the header
+	    // 'Access-Control-Allow-Credentials: true'.
+	    withCredentials: false
+	  },
+
+	  headers: {
+	    // Set any custom headers here.
+	    // If you set any non-simple headers, your server must include these
+	    // headers in the 'Access-Control-Allow-Headers' response header.
+	  },
+
+	  success: function() {
+	    // Here's where you handle a successful response.
+	    alert("yes!");
+	  },
+
+	  error: function() {
+	    // Here's where you handle an error response.
+	    // Note that if the error was due to a CORS issue,
+	    // this function will still fire, but there won't be any additional
+	    // information about the error.
+	  }
+	});
 }
